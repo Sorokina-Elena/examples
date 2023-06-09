95e4d4a856d0ce582f39b53411a55b37bd6ec289
«Elena <e.sorokina761@gmail.com>
2023-06-09 13:36:06 +0400

    Создали example008_friends_and_dog

---
+++ example008_friends_and_dog/Program.cs
@@ -0,0 +1,30 @@
+﻿int Distance = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int dogSpeed = 5;
+int count = 0;
+int friend = 2;
+int time = 0;
+
+
+
+do 
+{
+    if(friend == 1)
+    {
+        time = Distance/(FirstFriendSpeed + dogSpeed);
+        friend = 2;
+        Distance = Distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+        count = count + 1; 
+    }
+
+    else
+    {
+        time = Distance/(SecondFriendSpeed + dogSpeed); 
+        friend = 1;
+        Distance = Distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+        count = count + 1; 
+    }
+} 
+while(Distance > 10);
+Console.Write(count); 
