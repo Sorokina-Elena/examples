ca0d4a59eac87cb7957d900b1c2e1e5d5c8b0686
«Elena <e.sorokina761@gmail.com>
2023-06-12 10:53:53 +0400

    Настроили русский язык

--- example002_hello_user/Program.cs
+++ example002_hello_user/Program.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using System.Text;
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+System.Console.InputEncoding = Encoding.GetEncoding(1251);
+
 Console.WriteLine("Введите Ваше имя");
 string? username = Console.ReadLine();
 Console.WriteLine("Привет,");
